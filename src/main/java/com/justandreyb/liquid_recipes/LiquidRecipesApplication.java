2d1d78ea7ec4c42f783a2a8538738ef1938c430e
Andrey B <justandreyb@gmail.com>
2017-08-01 18:32:10 +0300

    Initial commit

---
+++ src/main/java/com/justandreyb/liquid_recipes/LiquidRecipesApplication.java
@@ -0,0 +1,12 @@
+package com.justandreyb.liquid_recipes;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LiquidRecipesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LiquidRecipesApplication.class, args);
+	}
+}
---
+++ src/test/java/com/justandreyb/liquid_recipes/LiquidRecipesApplicationTests.java
@@ -0,0 +1,16 @@
+package com.justandreyb.liquid_recipes;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class LiquidRecipesApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
